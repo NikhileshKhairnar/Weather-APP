4bf778446ed17bbf79f48e2eb30c9b1513b12194
Nikhilesh Khairnar <khairnar.v.nikhilesh@gmail.com>
2025-06-18 17:25:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const apiKey = "bdbc30ee6817b2b4f0c0c17e3eb1d652"
                 document.querySelector(".wind").innerHTML = data.wind.speed + " km/h"
 
                 if (data.weather[0].main == "Clouds") {
-                    weatherIcon.src = "images/clouds.png"
+                    weatherIcon.src = "clouds.png"
                 }
                 else if (data.weather[0].main == "Clear") {
-                    weatherIcon.src = "images/clear.png"
+                    weatherIcon.src = "clear.png"
                 }
                 else if (data.weather[0].main == "Rain") {
-                    weatherIcon.src = "images/rain.png"
+                    weatherIcon.src = "rain.png"
                 }
                 else if (data.weather[0].main == "Drizzle") {
-                    weatherIcon.src = "images/drizzle.png"
+                    weatherIcon.src = "drizzle.png"
                 }
                 else if (data.weather[0].main == "Mist") {
-                    weatherIcon.src = "images/mist.png"
+                    weatherIcon.src = "mist.png"
                 }
 
                 document.querySelector(".weather").style.display = "block"
